f4ed21680d3e4518692d76dc585dc9f6ff98c6d9
KeitaIsFree <maxdxd10108102@gmail.com>
2021-10-22 17:41:18 +0900

    [ADD] ワードごとのいいね数

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
   <ul id="example-1">
   <li v-for="(item, index) in words" :key="item">
     {{ item }}
-    <button v-on:click="goodCount[index] += 1">👍{{goodCount[index]}} </button>
+    <button v-on:click="$set(goodCount, index, goodCount[index]+1)">👍{{goodCount[index]}} </button>
   </li>
 </ul>
   </div>
@@ -17,6 +17,7 @@
       return {
         words: ['yuku', 'azami', 'pi-men'],
         goodCount: [0, 0, 0],
+	countReload: 0,
       }
     },
     async fetch() {
