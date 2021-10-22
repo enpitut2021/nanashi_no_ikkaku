a654f6533ea8f923dac72287d6a3b950673d5b38
misaki-lon <s1911360@s.tsukuba.ac.jp>
2021-10-22 16:13:53 +0900

    add 単語一覧の表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 <template>
 <div>
-  <h1>yuku</h1>
   <input v-model="message" placeholder="ワード">
   <button >追加</button>
+  <ul id="example-1">
+  <li v-for="item in words" :key="item">
+    {{ item }}
+  </li>
+</ul>
   </div>
 </template>
 
 <script>
-export default {}
+  export default {
+    data() {
+      return {
+        words: ['yuku', 'azami', 'pi-men']
+      }
+    },
+    async fetch() {
+
+    }
+  }
 </script>
