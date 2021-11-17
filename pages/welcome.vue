d6efe36c2c51622aac689cb6634594966eb0f55a
thistle519 <shizuru519@gmail.com>
2021-11-17 13:51:20 +0900

    add: インストラクションを書いた

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1>パッと思いつく単語をいれよう</h1>
     <input type="text" />
     <nuxt-link to="/">
       <button @click="submit">追加</button>
@@ -7,5 +8,6 @@
   </div>
 </template>
 <script>
+
 </script>
 
