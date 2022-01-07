7f841bdb8a3970123bff2f5b74703c37fc7d6506
KeitaIsFree <maxdxd10108102@gmail.com>
2022-01-07 16:27:56 +0900

    [ADD] デバッグ・リリースビルドかを最初にコンソールに出力

--- plugins/debug-tools.js
+++ plugins/debug-tools.js
@@ -1,6 +1,11 @@
 
 const isProduction = process.env.NODE_ENV === 'production'
 
+if (!isProduction)
+    console.log("ビルド：デバッグ（コンソール出力あり）")
+else
+    console.log("ビルド：リリース（コンソール出力なし）")
+
 // デバッグ時のみ出力するlog/error
 
 exports.log = (message, ...args) => {
