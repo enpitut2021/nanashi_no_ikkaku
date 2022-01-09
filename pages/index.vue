297694e8352ff91ae787bde3683e93bee04d1ffd
thistle519 <shizuru519@gmail.com>
2022-01-09 11:59:35 +0900

    add:scssが効いているかの確認テストを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,58 @@
 <template>
-  <div style="width: 100%; height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center">
-    <div style="max-width: 50%; text-align: center; font-size: 30px; margin: 30px">
+  <div
+    style="width: 100%; height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center"
+  >
+    <div
+      style="max-width: 50%; text-align: center; font-size: 30px; margin: 30px"
+    >
       <label>最近のマイブームは</label>
-      <input style="font-size: 30px" type="text" v-model="inputs[1]" size="10" placeholder="瞑想" />
+      <input
+        style="font-size: 30px"
+        type="text"
+        v-model="inputs[1]"
+        size="10"
+        placeholder="瞑想"
+      />
       <label>です！</label>
     </div>
     <nuxt-link to="/main">
       <button @click="submit">こんにちは</button>
     </nuxt-link>
+    <p>トマト色になるはずテスト</p>
   </div>
 </template>
+<style lang="scss">
+p {
+  color: tomato;
+}
+</style>
 <script>
-import firebase from "@/plugins/firebase"
-import dtools from "@/plugins/debug-tools.js"
+import firebase from "@/plugins/firebase";
+import dtools from "@/plugins/debug-tools.js";
 export default {
-	data() {
-		return {
-			inputs: [],
-		};
-	},
-	methods: {
-		submit() {
-			const db = firebase.firestore();
-			let dbWords = db.collection("test");
-			this.inputs.forEach(word => {
-				let inputWord = word;
-				if (inputWord != "") {
-					dbWords
-						.add({
-							word: inputWord,
-							good: 0,
-						})
-						.then((ref) => {
-							dtools.log("Add ID: ", ref.id);
-						});
-				}
-			});
-		},
-	},
+  data() {
+    return {
+      inputs: []
+    };
+  },
+  methods: {
+    submit() {
+      const db = firebase.firestore();
+      let dbWords = db.collection("test");
+      this.inputs.forEach(word => {
+        let inputWord = word;
+        if (inputWord != "") {
+          dbWords
+            .add({
+              word: inputWord,
+              good: 0
+            })
+            .then(ref => {
+              dtools.log("Add ID: ", ref.id);
+            });
+        }
+      });
+    }
+  }
 };
 </script>
-
