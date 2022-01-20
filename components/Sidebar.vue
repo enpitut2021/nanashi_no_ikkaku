a2af7bbccb4163e960a46c5e6fb2572e2c0ad533
Mattun1212 <s1911437@s.tsukuba.ac.jp>
2022-01-21 00:46:16 +0900

    [Add]次に進むボタンを押した時に名前の横に"Pressed"と出るようになった

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -8,7 +8,7 @@
     <p class="menu-label is-hidden-touch has-text-white is-size-6 has-text-weight-semibold">今話してる相手</p>
     <ul class="menu-list has-text-white">
       <li v-for="member in members" :key="member.id" class="mb-3">
-          {{member.member}}
+        <p>  {{member.member}} {{ (member.buttonStatus)? "Pressed" : ""}} </p>
       </li>
     </ul>
   </aside>
